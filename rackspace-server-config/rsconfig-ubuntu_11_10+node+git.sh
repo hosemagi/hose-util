69c067a9a7d37efc40f43486e4619f9e7bd7a03f
Preston <prestonclewis@gmail.com>
2012-03-24 15:17:12 -0700

    updates

--- rackspace-server-config/rsconfig-ubuntu_11_10+node+git.sh
+++ rackspace-server-config/rsconfig-ubuntu_11_10+node+git.sh
@@ -1,10 +1,41 @@
+#refresh apt-get packages
 sudo apt-get update
-sudo apt-get install git-core curl build-essential openssl libssl-dev pkg-config
-git clone https://github.com/joyent/node.git && cd node
-git checkout v0.6.3
+
+#install basics
+sudo apt-get -y install git-core curl build-essential openssl libssl-dev pkg-config
+
+#install node 0.6.3
+sudo git clone https://github.com/joyent/node.git && cd node
+sudo git checkout v0.6.3
 ./configure
 make
 sudo make install
 node -v
+
+#install npm
 curl http://npmjs.org/install.sh | sudo sh
 npm -v
+
+#install mongo
+sudo apt-get -y install mongodb
+
+#install mongolian
+sudo npm install mongolian
+
+#install nginx
+sudo apt-get -y install nginx
+
+#install forever
+sudo npm install forever
+
+#configure nginx front end proxy
+sudo cp ./nginx/site /etc/nginx/sites-available/site
+sudo ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled/site
+sudo /etc/init.d/nginx restart
+
+#create sysadmin user
+sudo useradd sysadmin
+sudo usermod -a -G admin sysadmin
+echo ** SETUP COMPLETE **
+echo ** Created sysadmin user, enter administrator password: **
+passwd sysadmin
